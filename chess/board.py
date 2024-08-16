2d68855bf28bf2f9ac0e019982a8ebbbf09da92e
Candela Gonzalez <cp.gonzalez@alumno.um.edu.ar>
2024-08-16 00:01:15 -0300

    posiciones en tablero piezas faltantes

--- chess/board.py
+++ chess/board.py
@@ -1,5 +1,9 @@
 from pieces import Rook
 from pieces import Bishop
+from pieces import Knight
+from pieces import King
+from pieces import Queen
+from pieces import Pawn
 
     class Board:
         def _init__ (self):
@@ -14,10 +18,26 @@ from pieces import Bishop
             self.__positions__[7] [7] = Rook('White') #torre blanca
             self.__positions__[0] [7] = Rook('Black') #torres negra2
             self.__positions__[7] [0] = Rook('White') #torre blanca2
+            
             self.__positions__[0] [2] = Bishop('Black') #alfil negro
             self.__positions__[0] [5] = Bishop('Black') #alfil negro2
             self.__positions__[7] [2] = Bishop('White') #alfil blanco
             self.__positions__[7] [5] = Bishop('White') #alfil blanco2
+
+            self.__positions__[0][4] = King('Black') #rey negro
+            self.__positions__[7][4] = King('White') #rey blanco
+
+            self.__positions__[0][3] =  Queen('Black') #reina negro
+            self.__positions__[7][3] = Queen('White') #reina blanca
+
+            self.__positions__[0][1] = Knight('Black') #caballo negro
+            self.__positions__[0][6] = Knight ('Black') #caballo negro2
+            self.__positions__[7][1] = Knight ('White') #caballo blanco
+            self.__positions__[7][6] = Knight ('White') #caballo blanco2
+
+        for i in range(8):
+            self.__positions__[1][i] = Pawn("BLACK")  #peones negros
+            self.__positions__[6][i] = Pawn("WHITE")  #peones blancos
 
         def get_piece(self, row, col):
             return self.__positions__ [row] [col]
