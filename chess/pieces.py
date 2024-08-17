65f17ba728bbbb622f471e2c39d83091c7f46105
Candela Gonzalez <cp.gonzalez@alumno.um.edu.ar>
2024-08-17 11:09:45 -0300

    separacion clases de cada pieza

--- chess/pieces.py
+++ chess/pieces.py
@@ -1,12 +1,3 @@
-class Piece:
+class Piece:                       #HERENCIA PIEZAS 
     def __init__(self, color):
         self.__color__ = color
-
-class Rook(Piece): #torre
-    ...
-
-class Pawn(Piece): #peon
-    ...
-
-class Bishop(Piece): #alfil
-    ...
