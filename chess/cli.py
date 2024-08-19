ff3db2699ee327600cdba8322ffbc77cb5c3aeba
Candela Gonzalez <cp.gonzalez@alumno.um.edu.ar>
2024-08-19 12:24:17 -0300

    obtener la pieza en una posicion y verificar que existe/ excepciones y turnos

--- chess/cli.py
+++ chess/cli.py
@@ -7,20 +7,33 @@ def main():
 
 def play (chess):
     try:
-        print(chess.show_board())     #muestro tablero
+        print(chess.show_board())                   #muestro tablero
         from_row = int(input('From row: '))
         from_col = int(input('From col: '))
         to_row = int(input('To Row: '))
         to_col = int(input('To Col: '))
 
-    #para decir desde cual casilla hasta cual casilla (movimientos)
-        chess.move(          
+        chess.move(                                #para decir desde cual casilla hasta cual casilla (movimientos)
             from_row,
             from_col,
             to_row,
             to_col
         )
 
-    #hago excepciones que garanticen que el camino esta bien
+            
+        piece = self.__board__.get_piece(from_row, from_col)          # Obtener la pieza en la posición de origen y verificar que existe
+        if piece is None:
+            raise ValueError("No existe una pieza en esa posición.")
+
+
+        if (self.__turn__ == "WHITE" and piece.color == "BLACK") or (self.__turn__ == "BLACK" and piece.color == "WHITE"):     #turnos
+            raise ValueError("No es tu turno.")
+   
+    
+    except ValueError as e:                 #hago excepciones que garanticen que el camino esta bien
+        print(f"Error: {e}")
+        return False
+
+
     except Excepcion as e:
         print ('error')
