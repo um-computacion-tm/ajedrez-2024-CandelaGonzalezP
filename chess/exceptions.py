ef649c1dfa7633af4767f6f18a0510ed3844a201
Candela Gonzalez <cp.gonzalez@alumno.um.edu.ar>
2024-08-21 12:19:23 -0300

    agrupacion de excepciones, agregado exc estandar y genericas

--- chess/exceptions.py
+++ chess/exceptions.py
@@ -1,8 +1,17 @@
-class InvalidMoveNoPiece(InvalidMove):
+#excepciones: siempre se va de la mas particular a la mas general (engloba)
+
+class InvalidMoveNoPiece(InvalidMove):                #Excepción para un movimiento desde una casilla vacía.
     ...
 
-class InvalidMoveRookMove(InvalidMove):
+class InvalidMoveRookMove(InvalidMove):               #Excepción para un movimiento inválido de la torre.
     ...
 
-class InvalidMove(Exception):
+class InvalidMove(Exception):                         # Clase base para excepciones de movimientos inválidos
     ...
+
+def handle_value_error(e):                            # Funciones para manejar excepciones estándar
+    print(f"Error: {e}")
+    return False
+
+def handle_generic_exception(e):
+    print('error')
