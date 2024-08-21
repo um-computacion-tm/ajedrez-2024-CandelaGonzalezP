f2e0e0a134d37e726c8ecaaf091f1ce5c01e35d3
Candela Gonzalez <cp.gonzalez@alumno.um.edu.ar>
2024-08-20 23:20:40 -0300

    rook actualizacion avance clases, sin modificar

--- chess/rook.py
+++ chess/rook.py
@@ -1,10 +1,15 @@
-from pieces import Piece       #TORRE CLASE
+from chess.pieces import Piece       #TORRE CLASE
 
 class Rook (Piece):
-    ...
 
     def __init__(self, position):
         self.__position__ = position
 
     def can_move(self, new_position):
         return (self.__position__[0] == new_position[0] or self.__position__[1] == new_position[1])     # La torre se mueve en la misma fila o columna
+
+    def __str__(self):
+        if self.__color__ == "WHITE":
+            return "♜"
+        else:
+            return "♖"
