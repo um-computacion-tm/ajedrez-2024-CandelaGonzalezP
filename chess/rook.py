521d6ed19ec492f600148cf993379cb3d9253690
Candela Gonzalez <cp.gonzalez@alumno.um.edu.ar>
2024-08-22 11:29:53 -0300

    movimientos Rook

--- chess/rook.py
+++ chess/rook.py
@@ -2,14 +2,38 @@ from chess.pieces import Piece       #TORRE CLASE
 
 class Rook (Piece):
 
-    def __init__(self, position):
+    def __init__(self, position, color):
+        super().__init__(color)
         self.__position__ = position
-
-    def can_move(self, new_position):
-        return (self.__position__[0] == new_position[0] or self.__position__[1] == new_position[1])     # La torre se mueve en la misma fila o columna
 
     def __str__(self):
         if self.__color__ == "WHITE":
             return "♜"
         else:
             return "♖"
+        
+    def get_possible_moves(self, board):
+        moves = []
+        x, y = self.__position__
+
+        for i in range(8):                                   # MOVIMIENTO HORIZONTAL
+            if i != x:                                       # No incluir la posición actual
+                if board.get_piece(i, y) is None:
+                    moves.append((i, y))
+                elif board.get_piece(i, y).__color__ != self.__color__:
+                    moves.append((i, y))
+                    break                                     # Detenerse si encuentra una pieza del color contrario
+                else:
+                    break                                     # Detenerse si encuentra una pieza del mismo color
+
+        for j in range(8):                                    # MOVIMIENTO VERTICAL
+            if j != y:                                        # No incluir la posición actual
+                if board.get_piece(x, j) is None:
+                    moves.append((x, j))
+                elif board.get_piece(x, j).__color__ != self.__color__:
+                    moves.append((x, j))
+                    break
+                else:
+                    break
+        
+        return moves
