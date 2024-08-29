31cd4fb3a0b401b34efe3c2f63ca17e356fdc654
Candela Gonzalez <cp.gonzalez@alumno.um.edu.ar>
2024-08-29 11:04:25 -0300

    movimientos de la torre nuevos, hechos en clase

--- chess/rook.py
+++ chess/rook.py
@@ -5,35 +5,62 @@ class Rook (Piece):
     def __init__(self, position, color):         #El constructor inicializa una nueva instancia de la torre con una posición en el tablero y un color.
         super().__init__(color)                  #Llama al constructor de la clase base Piece para inicializar el color de la pieza.
         self.__position__ = position             #Establece la posición inicial de la torre en el tablero.
-
-    def __str__(self):                           #Como la torre se muestra visualmente en el tablero cuando se imprime.
-        if self.__color__ == "WHITE":
-            return "♜"
-        else:
-            return "♖"
         
-    def get_possible_moves(self, board):          #Define movimientos posibles en el tablero
-        moves = []
-        x, y = self.__position__
+    white_str = "♜"                   #Como la torre se muestra visualmente en el tablero cuando se imprime.
+    black_str = "♖"
 
-        for i in range(8):                                   # MOVIMIENTO HORIZONTAL
-            if i != x:                                       # No incluir la posición actual
-                if board.get_piece(i, y) is None:
-                    moves.append((i, y))
-                elif board.get_piece(i, y).__color__ != self.__color__:
-                    moves.append((i, y))
-                    break                                     # Detenerse si encuentra una pieza del color contrario
-                else:
-                    break                                     # Detenerse si encuentra una pieza del mismo color
+    def valid_positions(
+        self,
+        from_row,
+        from_col,
+        to_row,
+        to_col,
+    ):
+        possible_positions = (
+            possible_positions_vd(from_row, from_col) +
+            possible_positions_va(from_row, from_col)
+        )
+        return (to_row, to_col) in possible_positions:
 
-        for j in range(8):                                    # MOVIMIENTO VERTICAL
-            if j != y:                                        # No incluir la posición actual
-                if board.get_piece(x, j) is None:
-                    moves.append((x, j))
-                elif board.get_piece(x, j).__color__ != self.__color__:
-                    moves.append((x, j))
-                    break
-                else:
-                    break
-        
-        return moves
+    def possible_positions_vd(self, row, col):
+        possibles = []
+        for next_row in range(row + 1, 8):
+            other_piece = self.__board__.get_piece(next_row, col)            # que la celda que sigue no este ocupada..
+            if other_piece is not None:
+                if other_piece.__color__ != self.__color__:
+                    possibles.append((next_row, col))
+                break
+            possibles.append((next_row, col))
+        return possibles
+
+    def possible_positions_va(self, row, col):
+        possibles = []
+        for next_row in range(row - 1, -1, -1):
+            possibles.append((next_row, col))
+        return possibles
+
+#    def get_possible_moves(self, board):          #Define movimientos posibles en el tablero
+#        moves = []
+#        x, y = self.__position__
+#
+#        for i in range(8):                                   # MOVIMIENTO HORIZONTAL
+#            if i != x:                                       # No incluir la posición actual
+#                if board.get_piece(i, y) is None:
+#                    moves.append((i, y))
+#                elif board.get_piece(i, y).__color__ != self.__color__:
+#                    moves.append((i, y))
+#                    break                                     # Detenerse si encuentra una pieza del color contrario
+#                else:
+#                    break                                     # Detenerse si encuentra una pieza del mismo color
+#
+#        for j in range(8):                                    # MOVIMIENTO VERTICAL
+#            if j != y:                                        # No incluir la posición actual
+#                if board.get_piece(x, j) is None:
+#                    moves.append((x, j))
+#                elif board.get_piece(x, j).__color__ != self.__color__:
+#                    moves.append((x, j))
+#                    break
+#                else:
+#                    break
+#        
+#        return moves
