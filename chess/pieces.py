b550abbdcad9024c7a41c2c9f1d9163ddd38d3d1
Candela Gonzalez <cp.gonzalez@alumno.um.edu.ar>
2024-08-29 11:03:46 -0300

    cambios hechos en clase, Pieces

--- chess/pieces.py
+++ chess/pieces.py
@@ -1,3 +1,11 @@
 class Piece:                       #HERENCIA PIEZAS 
-    def __init__(self, color):
+
+    def __init__(self, color, board):
         self.__color__ = color
+        self.__board__ = board
+
+    def __str__(self):
+        if self.__color__ == "WHITE":
+            return self.white_str
+        else:
+            return self.black_str
