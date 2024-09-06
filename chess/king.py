a172f842a329cd9b49b2454c56ef8d7d5d51ed86
Candela Gonzalez <cp.gonzalez@alumno.um.edu.ar>
2024-09-06 12:56:06 -0300

    piezas rey

--- chess/king.py
+++ chess/king.py
@@ -1,4 +1,9 @@
 from chess.pieces import Piece        #REY CLASE
 
 class King(Piece):
-    ...
+
+    def __str__(self):  
+      if self.__color__ == "WHITE":
+          return "♚"
+      else:
+         return "♔" 
