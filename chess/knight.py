19bcf56c73b1b562c34fd54290dfe14a376ee45d
Candela Gonzalez <cp.gonzalez@alumno.um.edu.ar>
2024-09-17 00:02:29 -0300

    agregado funciones clase knight

--- chess/knight.py
+++ chess/knight.py
@@ -4,5 +4,10 @@ class Knight(Piece):
 
     white_str ="♞"
     black_str ="♘" 
-      
+
+
+    def get_possible_positions(self, from_row, from_col):
+        return ()
+
+
 #hacer codigo directamente aca (no se puede reutilizar codigo)
