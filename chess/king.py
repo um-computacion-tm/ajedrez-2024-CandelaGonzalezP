4f7e584460fd5f3a4bfbf5bbb72d31e03720fb7e
Candela Gonzalez <cp.gonzalez@alumno.um.edu.ar>
2024-09-19 12:10:35 -0300

    optimizando codigo

--- chess/king.py
+++ chess/king.py
@@ -5,6 +5,23 @@ class King(Piece):
     white_str ="♚"
     black_str = "♔" 
 
+    def valid_positions(
+        self,
+        from_row,
+        from_col,
+        to_row,
+        to_col,
+    ):
+
+        possible_positions = (
+            self.possible_positions_vd(from_row, from_col) +  # verticales hacia abajo
+            self.possible_positions_va(from_row, from_col) +  # verticales hacia arriba
+            self.possible_positions_hr(from_row, from_col) +  # horizontales hacia la derecha
+            self.possible_positions_hl(from_row, from_col)    # horizontales hacia la izquierda
+        )
+        return (to_row, to_col) in possible_positions
+
+
     def get_possible_positions(self, from_row, from_col):
         possibles = self.possible_orthogonal_positions(
             from_row,
