ad6101d994854f113aaa9c5012498902ab361539
Candela Gonzalez <cp.gonzalez@alumno.um.edu.ar>
2024-09-20 15:59:45 -0300

    test alfil cambios y agregados (por cambios codigo pieces)

--- tests/testAlfil.py
+++ tests/testAlfil.py
@@ -1,48 +1,158 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""import unittest
+import unittest
 from chess.bishop import Bishop
 from chess.board import Board
+from chess.pawn import Pawn
 
 class TestBishop(unittest.TestCase):
 
     def setUp(self):
-        self.board = Board()
+        self.board = Board(for_test=True)  # Inicializa el tablero sin piezas
+        self.bishop = Bishop('White', self.board)  # Crea un alfil blanco
 
-    def test_bishop_moves(self):
-        bishop = Bishop((2, 3), "WHITE")
-        self.board.place_piece(bishop, (2, 3))
-        moves = bishop.get_possible_moves(self.board)
+# testeo posiciones posibles
 
-        # Movimientos esperados en un tablero vacío
-        expected_moves = [
-            (3, 4), (4, 5), (5, 6), (6, 7),  # Diagonal arriba derecha
-            (3, 2), (4, 1), (5, 0),          # Diagonal arriba izquierda
-            (1, 4), (0, 5),                  # Diagonal abajo derecha
-            (1, 2), (0, 1)                   # Diagonal abajo izquierda
-        ]
-        self.assertEqual(set(moves), set(expected_moves))
+    def test_get_possible_positions(self):
+        self.board.set_piece(3, 3, self.bishop)  # Coloca el alfil en (3, 3)
+        result = self.bishop.get_possible_positions(3, 3)
+        expected = self.bishop.possible_diagonal_positions(3, 3)
+        self.assertEqual(result, expected)
+
+
+# testeo movimientos diagonales ascendentes
+
+    
+    def test_possible_positions_dad(self):
+        board = Board(for_test=True)
+        bishop = Bishop("WHITE", board)
+        board.set_piece(4, 4, bishop)
+        
+        # Caso sin bloqueos
+        possibles = bishop.possible_positions_dad(4, 4)
+        expected_positions = [(3, 5), (2, 6), (1, 7)]
+        self.assertEqual(possibles, expected_positions)
+
+    def test_possible_positions_dad_with_block(self):
+        board = Board(for_test=True)
+        bishop = Bishop("WHITE", board)
+        board.set_piece(4, 4, bishop)
+        
+        # Bloqueo con una pieza enemiga
+        board.set_piece(2, 6, Pawn("BLACK", board))
+        possibles = bishop.possible_positions_dad(4, 4)
+        expected_positions = [(3, 5), (2, 6)]  # Se detiene en la pieza enemiga
+        self.assertEqual(possibles, expected_positions)
+
+    def test_possible_positions_dad_with_own_piece_block(self):
+        board = Board(for_test=True)
+        bishop = Bishop("WHITE", board)
+        board.set_piece(4, 4, bishop)
+        
+        # Bloqueo con una pieza propia
+        board.set_piece(3, 5, Pawn("WHITE", board))
+        possibles = bishop.possible_positions_dad(4, 4)
+        expected_positions = []  # Se detiene en la propia pieza
+        self.assertEqual(possibles, expected_positions)
+
+
+    def test_possible_positions_dai(self):
+        board = Board(for_test=True)
+        bishop = Bishop("WHITE", board)
+        board.set_piece(4, 4, bishop)
+        
+        # Caso sin bloqueos
+        possibles = bishop.possible_positions_dai(4, 4)
+        expected_positions = [(3, 3), (2, 2), (1, 1), (0, 0)]
+        self.assertEqual(possibles, expected_positions)
+
+    def test_possible_positions_dai_with_block(self):
+        board = Board(for_test=True)
+        bishop = Bishop("WHITE", board)
+        board.set_piece(4, 4, bishop)
+        
+        # Bloqueo con una pieza enemiga
+        board.set_piece(2, 2, Pawn("BLACK", board))
+        possibles = bishop.possible_positions_dai(4, 4)
+        expected_positions = [(3, 3), (2, 2)]  # Se detiene en la pieza enemiga
+        self.assertEqual(possibles, expected_positions)
+
+    def test_possible_positions_dai_with_own_piece_block(self):
+        board = Board(for_test=True)
+        bishop = Bishop("WHITE", board)
+        board.set_piece(4, 4, bishop)
+        
+        # Bloqueo con una pieza propia
+        board.set_piece(3, 3, Pawn("WHITE", board))
+        possibles = bishop.possible_positions_dai(4, 4)
+        expected_positions = []  # Se detiene en la propia pieza
+        self.assertEqual(possibles, expected_positions)
+
+
+# testeo movimientos diagonales descendentes
+
+    def test_possible_positions_ddd(self):
+        board = Board(for_test=True)
+        bishop = Bishop("WHITE", board)
+        board.set_piece(4, 4, bishop)
+        
+        # Caso sin bloqueos
+        possibles = bishop.possible_positions_ddd(4, 4)
+        expected_positions = [(5, 5), (6, 6), (7, 7)]
+        self.assertEqual(possibles, expected_positions)
+
+    def test_possible_positions_ddd_with_block(self):
+        board = Board(for_test=True)
+        bishop = Bishop("WHITE", board)
+        board.set_piece(4, 4, bishop)
+        
+        # Bloqueo con una pieza enemiga
+        board.set_piece(6, 6, Pawn("BLACK", board))
+        possibles = bishop.possible_positions_ddd(4, 4)
+        expected_positions = [(5, 5), (6, 6)]  # Se detiene en la pieza enemiga
+        self.assertEqual(possibles, expected_positions)
+
+    def test_possible_positions_ddd_with_own_piece_block(self):
+        board = Board(for_test=True)
+        bishop = Bishop("WHITE", board)
+        board.set_piece(4, 4, bishop)
+        
+        # Bloqueo con una pieza propia
+        board.set_piece(5, 5, Pawn("WHITE", board))
+        possibles = bishop.possible_positions_ddd(4, 4)
+        expected_positions = []  # Se detiene en la propia pieza
+        self.assertEqual(possibles, expected_positions)
+
+    def test_possible_positions_ddi(self):
+        board = Board(for_test=True)
+        bishop = Bishop("WHITE", board)
+        board.set_piece(4, 4, bishop)
+
+        # Caso sin bloqueos
+        possibles = bishop.possible_positions_ddi(4, 4)
+        expected_positions = [(5, 3), (6, 2), (7, 1)]
+        self.assertEqual(possibles, expected_positions)
+
+    def test_possible_positions_ddi_with_block(self):
+        board = Board(for_test=True)
+        bishop = Bishop("WHITE", board)
+        board.set_piece(4, 4, bishop)
+
+        # Bloqueo con una pieza enemiga
+        board.set_piece(6, 2, Pawn("BLACK", board))
+        possibles = bishop.possible_positions_ddi(4, 4)
+        expected_positions = [(5, 3), (6, 2)]  # Se detiene en la pieza enemiga
+        self.assertEqual(possibles, expected_positions)
+
+    def test_possible_positions_ddi_with_own_piece_block(self):
+        board = Board(for_test=True)
+        bishop = Bishop("WHITE", board)
+        board.set_piece(4, 4, bishop)
+
+        # Bloqueo con una pieza propia
+        board.set_piece(5, 3, Pawn("WHITE", board))
+        possibles = bishop.possible_positions_ddi(4, 4)
+        expected_positions = []  # Se detiene en la propia pieza
+        self.assertEqual(possibles, expected_positions)
+
 
 if __name__ == '__main__':
-    unittest.main()"""
+    unittest.main()
