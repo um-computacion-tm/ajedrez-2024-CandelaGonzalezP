b3179cb4fdb40f828df45c2aa21644b3bc4a83a9
Candela Gonzalez <cp.gonzalez@alumno.um.edu.ar>
2024-09-22 15:35:24 -0300

    agregado movimientos caballos

--- chess/knight.py
+++ chess/knight.py
@@ -1,18 +1,5 @@
 from chess.pieces import Piece      #CABALLO CLASE
 
-"""
-class Knight(Piece):
-
-    white_str ="♞"
-    black_str ="♘" 
-
-
-    def get_possible_positions(self, from_row, from_col):
-        return ()
-"""
-
-
-
 class Knight(Piece):
     def __init__(self, color, position=None):
         super().__init__(color, position)
@@ -27,6 +14,19 @@ class Knight(Piece):
         to_row, to_col = to_pos
         return (abs(from_row - to_row) == 2 and abs(from_col - to_col) == 1) or \
                (abs(from_row - to_row) == 1 and abs(from_col - to_col) == 2)
-    
-    
+
+
+  
+"""
+class Knight(Piece):
+
+    white_str ="♞"
+    black_str ="♘" 
+
+
+    def get_possible_positions(self, from_row, from_col):
+        return ()
+"""  
+
+
 #hacer codigo directamente aca (no se puede reutilizar codigo) - (hecho)
