ae2da8f8994b9ffa75dbb4cbb8394c73260a6256
Candela Gonzalez <cp.gonzalez@alumno.um.edu.ar>
2024-09-22 15:34:08 -0300

    agregado movimientos caballos

--- chess/knight.py
+++ chess/knight.py
@@ -1,5 +1,6 @@
 from chess.pieces import Piece      #CABALLO CLASE
 
+"""
 class Knight(Piece):
 
     white_str ="♞"
@@ -8,6 +9,24 @@ class Knight(Piece):
 
     def get_possible_positions(self, from_row, from_col):
         return ()
+"""
 
 
-#hacer codigo directamente aca (no se puede reutilizar codigo)
+
+class Knight(Piece):
+    def __init__(self, color, position=None):
+        super().__init__(color, position)
+        
+    def symbol(self):
+        return 'N' if self.get_color() == "WHITE" else 'n'    
+        
+    def is_valid_piece_move(self, board, from_pos, to_pos):
+        """Verificar si el movimiento es válido para el caballo."""
+        """El caballo se mueve en forma de "L". """
+        from_row, from_col = from_pos
+        to_row, to_col = to_pos
+        return (abs(from_row - to_row) == 2 and abs(from_col - to_col) == 1) or \
+               (abs(from_row - to_row) == 1 and abs(from_col - to_col) == 2)
+    
+    
+#hacer codigo directamente aca (no se puede reutilizar codigo) - (hecho)
