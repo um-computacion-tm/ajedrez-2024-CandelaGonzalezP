ff8661536c79eacd654f19a77385bb5ccaeaddf0
Candela Gonzalez <cp.gonzalez@alumno.um.edu.ar>
2024-10-02 12:52:17 -0300

    test king

--- tests/testRey.py
+++ tests/testRey.py
@@ -16,47 +16,72 @@ class TestKing(unittest.TestCase):
         king = King("BLACK", board)
         self.assertEqual(king.symbol(), 'k')
 
-# movimientos
+# Movimientos válidos
 
-    def setUp(self):
-        self.board = Board(for_test=True)
-        
-    def test_king_move_one_square(self):
-        king = King("WHITE", self.board)
-        self.board.set_piece(4, 4, king)
-        # Verifica si el rey puede moverse una casilla en diagonal
-        is_valid = king.valid_positions(4, 4, 5, 5)
-        self.assertFalse(is_valid)
+    def test_move_up(self):
+        board = Board()
+        king = King("WHITE", board)
+        from_pos = (4, 4)
+        to_pos = (3, 4)  # Una casilla hacia arriba
+        self.assertTrue(king.is_valid_piece_move(board, from_pos, to_pos))
 
-    def test_king_blocked_by_own_piece(self):
-        king = King("WHITE", self.board)
-        self.board.set_piece(4, 4, king)
-        self.board.set_piece(5, 5, King("WHITE", self.board))  # Pieza blanca bloqueando
-        # Verifica que el rey no pueda moverse si hay una pieza propia bloqueando
-        is_valid = king.valid_positions(4, 4, 5, 5)
-        self.assertFalse(is_valid)
+    def test_move_down(self):
+        board = Board()
+        king = King("WHITE", board)
+        from_pos = (4, 4)
+        to_pos = (5, 4)  # Una casilla hacia abajo
+        self.assertTrue(king.is_valid_piece_move(board, from_pos, to_pos))
 
-    def test_king_move_two_squares_invalid(self):
-        king = King("WHITE", self.board)
-        self.board.set_piece(4, 4, king)
-        # El rey no debería poder moverse dos casillas (movimiento inválido)
-        is_valid = king.valid_positions(4, 4, 6, 4)
-        self.assertTrue(is_valid)
+    def test_move_right(self):
+        board = Board()
+        king = King("WHITE", board)
+        from_pos = (4, 4)
+        to_pos = (4, 5)  # Una casilla hacia la derecha
+        self.assertTrue(king.is_valid_piece_move(board, from_pos, to_pos))
 
-    def test_king_move_too_far(self):
-        king = King("WHITE", self.board)
-        self.board.set_piece(4, 4, king)
-        is_valid = king.valid_positions(4, 4, 6, 6)  # Más de un paso
-        self.assertFalse(is_valid)
+    def test_move_left(self):
+        board = Board()
+        king = King("WHITE", board)
+        from_pos = (4, 4)
+        to_pos = (4, 3)  # Una casilla hacia la izquierda
+        self.assertTrue(king.is_valid_piece_move(board, from_pos, to_pos))
 
+    def test_move_up_right(self):
+        board = Board()
+        king = King("WHITE", board)
+        from_pos = (4, 4)
+        to_pos = (3, 5)  # Diagonal hacia arriba a la derecha
+        self.assertTrue(king.is_valid_piece_move(board, from_pos, to_pos))
 
+    def test_move_down_left(self):
+        board = Board()
+        king = King("WHITE", board)
+        from_pos = (4, 4)
+        to_pos = (5, 3)  # Diagonal hacia abajo a la izquierda
+        self.assertTrue(king.is_valid_piece_move(board, from_pos, to_pos))
 
+# Movimientos inválidos
 
+    def test_invalid_move_up(self):
+        board = Board()
+        king = King("WHITE", board)
+        from_pos = (4, 4)
+        to_pos = (2, 4)  # Dos casillas hacia arriba
+        self.assertFalse(king.is_valid_piece_move(board, from_pos, to_pos))
 
+    def test_invalid_diagonal_move(self):
+        board = Board()
+        king = King("WHITE", board)
+        from_pos = (4, 4)
+        to_pos = (2, 2)  # Dos casillas en diagonal
+        self.assertFalse(king.is_valid_piece_move(board, from_pos, to_pos))
 
-
-
-
+    def test_invalid_horizontal_jump(self):
+        board = Board()
+        king = King("WHITE", board)
+        from_pos = (4, 4)
+        to_pos = (4, 6)  # Dos casillas hacia la derecha
+        self.assertFalse(king.is_valid_piece_move(board, from_pos, to_pos))
 
 
 if __name__ == '__main__':
