f1870670a75f05556ab47ad74a537e5c909812ac
Candela Gonzalez <cp.gonzalez@alumno.um.edu.ar>
2024-10-14 00:13:35 -0300

    arreglos varios

--- chess/queen.py
+++ chess/queen.py
@@ -5,20 +5,14 @@ class Queen(Piece):
     def symbol(self):
         return 'Q' if self.get_color() == "WHITE" else 'q'
 
-def queen_valid_positions(self, move, directions=None):
-    """Verifica si el movimiento del rey es válido."""
-    from_row, from_col, to_row, to_col = move['from_row'], move['from_col'], move['to_row'], move['to_col']
-    if directions is None:
-        directions = self.__king_queen_directions__
-    
-    possible_positions = self.find_valid_moves(from_row, from_col, directions, single_step=False)
-    return (to_row, to_col) in possible_positions
-
-    """def queen_valid_positions(self, from_row, from_col, to_row, to_col, directions=None):
+    def queen_valid_positions(self, move, directions=None):
+        """Verifica si el movimiento del rey es válido."""
+        from_row, from_col, to_row, to_col = move['from_row'], move['from_col'], move['to_row'], move['to_col']
         if directions is None:
             directions = self.__king_queen_directions__
+        
         possible_positions = self.find_valid_moves(from_row, from_col, directions, single_step=False)
-        return (to_row, to_col) in possible_positions"""
+        return (to_row, to_col) in possible_positions
 
 
 #uso herencia
--- tests/testChess.py
+++ tests/testChess.py
@@ -1,4 +1,4 @@
-"""import unittest
+import unittest
 from chess.chess import Chess
 from chess.exceptions import *
 from chess.board import Board
@@ -13,17 +13,17 @@ class TestChess(unittest.TestCase):
 
         self.piece = MagicMock()
 
-
     def test_is_playing(self):
         self.assertTrue(self.chess.is_playing())  # Verifica que is_playing retorne True
 
-
     def test_move_valid(self):
-        self.assertEqual(self.chess.turn, "White")        # Verifica el turno inicial
-        with self.assertRaises(InvalidMove):        # Intenta realizar un movimiento inválido
-            self.chess.move(7, 2, 5, 4)  # Movimiento inválido
-        self.assertEqual(self.chess.turn, "White")        # Verifica que el turno sigue siendo "white" (no cambió)
+        self.assertEqual(self.chess.turn, "White")  # Verifica el turno inicial
+        self.chess.__board__.get_piece.return_value = self.piece  # Simula una pieza en el tablero
 
+        with self.assertRaises(InvalidMove):  # Intenta realizar un movimiento inválido
+            self.chess.move(7, 2, 5, 4)  # Movimiento inválido
+
+        self.assertEqual(self.chess.turn, "White")  # Verifica que el turno sigue siendo "white"
 
     def test_change_turn(self):
         # Verifica el turno inicial
@@ -37,18 +37,15 @@ class TestChess(unittest.TestCase):
         # Verifica que el turno cambió de nuevo a 'white'
         self.assertEqual(self.chess.turn, "black")
 
-
     def test_ganador_when_opponent_has_no_pieces(self):
         self.chess.__board__.count_pieces.return_value = 0  # Simula que no quedan piezas del oponente
         result = self.chess.ganador()  # Llama al método
         self.assertTrue(result)  # Verifica que retorna True cuando el oponente no tiene piezas
 
-
     def test_ganador_when_opponent_has_pieces(self):
         self.chess.__board__.count_pieces.return_value = 1  # Simula que quedan piezas del oponente
         result = self.chess.ganador()  # Llama al método
         self.assertFalse(result)  # Verifica que retorna False cuando el oponente tiene piezas
-
 
     def test_move_empty_position(self):
         # Configura el mock para que devuelva None al intentar obtener la pieza de la posición (0, 0)
@@ -68,49 +65,40 @@ class TestChess(unittest.TestCase):
             self.chess.move(1, 0, 8, 0)  # Movimiento inválido hacia fuera del tablero
         self.assertEqual(str(context.exception), "Movimiento inválido hacia la posición de destino")  # Verifica el mensaje de la excepción
 
-
     @patch('builtins.input', side_effect=['si'])  # Simula que el usuario ingresa "si"
     def test_offer_draw_accept(self, mock_input):
-        game = Chess()
         with self.assertRaises(SystemExit):  # Espera que se llame a sys.exit()
-            game.offer_draw()  # Llama al método
-
+            self.chess.offer_draw()  # Llama al método
 
     @patch('builtins.input', side_effect=['no'])  # Simula que el usuario ingresa "no"
     def test_offer_draw_reject(self, mock_input):
-        game = Chess()
-        result = game.offer_draw()  # Llama al método
+        result = self.chess.offer_draw()  # Llama al método
         self.assertTrue(result)  # Espera que la partida continúe
-
 
     @patch.object(Chess, 'check_valid_move')
     @patch.object(Chess, 'check_turn')  # Mockea check_turn para evitar InvalidTurn
     def test_check_valid_move_called(self, mock_check_turn, mock_check_valid_move):
-        #Verifica que se llame a check_valid_move correctamente.
+        # Verifica que se llame a check_valid_move correctamente.
         from_row, from_col, to_row, to_col = 1, 1, 2, 2
         mock_check_valid_move.return_value = None  # Simula que el movimiento es válido
         mock_check_turn.return_value = None  # Simula que el turno es válido
 
         # Llama al método validate_move
-        self.chess.validate_move(self.piece, from_row, from_col, to_row, to_col)
+        self.chess.validate_move(self.piece, {'from_row': from_row, 'from_col': from_col, 'to_row': to_row, 'to_col': to_col})
 
         # Verifica que check_valid_move fue llamado con los argumentos correctos
-        mock_check_valid_move.assert_called_once_with(self.piece, from_row, from_col, to_row, to_col)
+        mock_check_valid_move.assert_called_once_with(self.piece, {'from_row': from_row, 'from_col': from_col}, to_row, to_col)
 
     @patch.object(Chess, 'check_valid_move')
     @patch.object(Chess, 'check_turn')  # Mockea check_turn para evitar InvalidTurn
     def test_invalid_move(self, mock_check_turn, mock_check_valid_move):
-        #Verifica que se lanza InvalidMove si el movimiento no es válido.
+        # Verifica que se lanza InvalidMove si el movimiento no es válido.
         from_row, from_col, to_row, to_col = 1, 1, 2, 2
         mock_check_turn.return_value = None  # Simula que el turno es válido
         mock_check_valid_move.side_effect = InvalidMove("Movimiento inválido")  # Simula un movimiento inválido
 
         with self.assertRaises(InvalidMove):
-            self.chess.validate_move(self.piece, from_row, from_col, to_row, to_col)
-
-
-
+            self.chess.validate_move(self.piece, {'from_row': from_row, 'from_col': from_col, 'to_row': to_row, 'to_col': to_col})
 
 if __name__ == '__main__':
     unittest.main()
-"""
